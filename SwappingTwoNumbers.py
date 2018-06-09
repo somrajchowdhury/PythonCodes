11797d15364fb66614c4f66c5aedaf87547d7e1b
Somraj Chowdhury <39940462+somrajchowdhury@users.noreply.github.com>
2018-06-10 01:23:03 +0530

    Add files via upload

---
+++ SwappingTwoNumbers.py
@@ -0,0 +1,11 @@
+print('Before Swapping')
+n1 = eval(input('first number: '))      #Input first number
+n2 = eval(input('second number: '))     #Input second number
+print()
+print('Swapping...')
+n1,n2 = n2,n1           #Main Logic of Swapping - Simultaneous Assignment
+print()
+print('After Swapping')
+print('first number: ',n1)
+print('second number: ',n2)
+
