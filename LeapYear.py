fbefdf10529001c047526f0c5ef92c2fbcf48406
Somraj Chowdhury <39940462+somrajchowdhury@users.noreply.github.com>
2018-06-10 18:13:02 +0530

    Add files via upload

---
+++ LeapYear.py
@@ -0,0 +1,42 @@
+year=int(input("Enter a year: "))
+
+'''
+A year is leap year if 
+1) Year is multiple of 4 and not multiple of 100.
+2) Year is multiple of 400
+'''
+
+if ((year%4==0 and year%100!=0) or (year%400==0)):        
+    print(str(year) + ' is a leap year!')
+    print('366 days')
+    print('''
+          January   : 31
+          February  : 29
+          March     : 31
+          April     : 30
+          May       : 31
+          June      : 30
+          July      : 31
+          August    : 31
+          September : 30
+          October   : 31
+          November  : 30
+          December  : 31    
+    ''')
+else:
+    print(str(year) + ' is not a leap year!')
+    print('365 Days')
+    print('''
+              January   : 31
+              February  : 28
+              March     : 31
+              April     : 30
+              May       : 31
+              June      : 30
+              July      : 31
+              August    : 31
+              September : 30
+              October   : 31
+              November  : 30
+              December  : 31
+        ''')
