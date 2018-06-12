96cd1a32ccab3716b21bfe80758b53437ce5f8dd
Somraj Chowdhury <39940462+somrajchowdhury@users.noreply.github.com>
2018-06-13 01:11:12 +0530

    Update SecondLargestNumber.py

--- SecondLargestNumber.py
+++ SecondLargestNumber.py
@@ -4,12 +4,12 @@ list = []       #Create empty list
 
 for i in range(1,n+1):  
     elem = int(input('Element '+str(i)+' : '))      #Enter the elements of list
-    list.append(elem)       #Add these elements to the list using append()
+    list.append(elem)                               #Add these elements to the list using append()
     
-print()     #Print nothing, return an empty line
+print()                                 #Print nothing, return an empty line
 print('List = ' + str(list) + '\n')     #Display the list elements
 
-list.sort()     #Sort the list elements in increasing order using sort()
+list.sort()                                     #Sort the list elements in increasing order using sort()
 print('Sorted List = ' + str(list) + '\n')      #Display sorted list elements
 
 count = 0
